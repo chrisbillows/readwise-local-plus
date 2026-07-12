7814879e13fe2b51d508f9b468b7271e6742c12f
Chris Billows <96173696+chrisbillows@users.noreply.github.com>
2026-07-12 12:41:42 +0100

    ensure req folders

--- readwise_local_plus/integrations/obsidian.py
+++ readwise_local_plus/integrations/obsidian.py
@@ -5,25 +5,25 @@ from readwise_local_plus.config import UserConfig
 from pathlib import Path
 
 
-def get_dirs(target_path: Path) -> str:
-    paths = target_path.iterdir()
-    
-    dirs = []
-    for path in paths:
-        if path.is_dir():
-            dirs.append(path)
+REQUIRED_PARENT_FOLDERS = ["podcasts"]
 
-    print("--- Dirs only ---")
-    for dir in dirs:
-        print(dir)
+
+logger = logging.getLogger(__name__)
+
+
+def ensure_rw_parent_folders(
+        user_config: UserConfig, required_folders: list[int] = REQUIRED_PARENT_FOLDERS
+    ) -> None:
+    for required_folder in REQUIRED_PARENT_FOLDERS:
+        expected_path = user_config.obsidian_vault_path / required_folder
+        if not expected_path.is_dir():
+            expected_path.mkdir() # Error if exists, or parents don't exist
+            logger.info(f"Created required folder: {expected_path}")
 
 
 def obsidian_experiment(user_config: UserConfig, batch_id: int):
     print("Hello Obsidian Export!")
-    print("Batch ID is ", batch_id)
-    print("---")
-    print(user_config)
-    print("---")
-    get_dirs(user_config.obsidian_vault_path)
+    # obs_root_dirs = get_dirs(user_config.obsidian_vault_path)
+    ensure_rw_parent_folders(user_config)
 
 
