135ea5ffa2b822613a5170064b9b6d7436ea2da1
Chris Billows <96173696+chrisbillows@users.noreply.github.com>
2026-09-02 13:13:56 +0100

    improve end-to-end flow

--- readwise_local_plus/workflows/obsidian/obsidian_snipd.py
+++ readwise_local_plus/workflows/obsidian/obsidian_snipd.py
@@ -51,49 +51,70 @@ def ensure_readwise_dirs(
 
 
 
-def write_batch_to_obsidian(user_config: UserConfig, batch_id: int):
-    """Entry point function to write a batch of highlights to Obsidian."""
+def write_dbhls_to_obsidian(
+        user_config: UserConfig, query_shortname: str, batch_id: int | str,
+    ) -> None:
+    """
+    Entry point function to write a batch of highlights to Obsidian.
+
+    Parameters
+    ----------
+    user_config: UserConfig
+        A RWLP user config object.
+    query_shortname: str
+        A query shortname that appears in `db_export.DbHls.DB_QUERIES`
+    batch_id: int | str
+        A batch_id to "all" for all Hls meeting the query, regardless of
+        batch.
+    """
     ensure_readwise_dirs(user_config)
 
-    db_hls = DbHls("all_snipd")
-    db_hls.populate()
+    dbhls = DbHls(query_shortname, batch_id)
 
     # TODOs: Use analysis obj if still needed
     # print_highlight_type_stats(batch_hls_by_book)
 
-    for snipd_ep in db_hls.hls_by_snipd_url:
+    for snipd_ep in dbhls.hls_by_snipd_url:
 
         snipd_ep.populate()
 
-        if not snipd_ep.full_page:
-            print("UH OH NO FULL PAGE BRO..")
+        # `podcasts` aka BookFromDb.category is hardcoded for consistency
+        podcast_dir = user_config.obsidian_rw_dir / "podcasts" / snipd_ep.podcast_title
+        ensure_dir_exists(podcast_dir)
 
+        episode_file = podcast_dir / (snipd_ep.episode_title + ".md")
+
+        breakpoint()
+
+        # create new file
+        if not episode_file.exists():
+            episode_file.write_text(snipd_ep.full_page)
+            logger.info(f"Episode created:: {episode_file.name}")
+
+        # append to existing file
         else:
-            # `podcasts` aka BookFromDb.category is hardcoded for consistency
-            podcast_dir = user_config.obsidian_rw_dir / "podcasts" / snipd_ep.podcast_title
-            ensure_dir_exists(podcast_dir)
-
-            episode_file = podcast_dir / (snipd_ep.episode_title + ".md")
-            # create new file 
-            if not episode_file.exists():
-                episode_file.write_text(snipd_ep.full_page)
-                logger.info(f"Episode created:: {episode_file.name}")
-
-            # append to existing file
-            else:
-                # Use `open` as cannot append with pathlib
-                with open(episode_file, "a") as file_handle:
-                    episode_content = (
-                        f"\n\n***(Appended {str(date.today())})***\n\n" +
-                        snipd_ep.page_body
-                    ) 
-                    file_handle.write(episode_content)
-                logger.info(f"Episode appended: {episode_file.name}")
+            # Use `open` as cannot append with pathlib
+            with open(episode_file, "a") as file_handle:
+                episode_content = (
+                    f"\n\n***(Appended {str(date.today())})***\n\n" +
+                    snipd_ep.page_body
+                )
+                file_handle.write(episode_content)
+            logger.info(f"Episode appended: {episode_file.name}")
 
 
 if __name__ == "__main__":
     from readwise_local_plus.config import fetch_user_config
 
     user_config = fetch_user_config()
+    query_shortname = "snipd"
 
-    write_batch_to_obsidian(user_config, 67)
+    # Useful batches
+    batch_id = "all" # 890
+    # batch_id = 110 # 0 books
+    # batch_id = 111 # 376 books
+    # batch_id = 112 # 1 book
+    # batch_id = 113 # 10 books
+
+    write_dbhls_to_obsidian(user_config, query_shortname, batch_id)
+
